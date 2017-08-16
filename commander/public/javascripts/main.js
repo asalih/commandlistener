7c4b772ad44e4e9d07396f809676909c7b57d516
Ahmet Salih <ahmet.salih@windowslive.com>
2017-08-16 09:45:45 +0300

    init

---
+++ commander/public/javascripts/main.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    $(".commandInput").keyup(function (t) {
+        if (t.keyCode == 13) {
+            var value = $(this).val();
+            putCommand(value);
+            $(".commands").append("<p>" + value + "</p>");
+        }
+    });
+
+
+    var working = false;
+    window.setInterval(function () {
+        if (document.hidden) { return;}
+        if (working) {
+            return;
+        }
+        working = true;
+
+        $.ajax({
+            url: '/any-response',
+            type: "GET",
+            contentType: "text/plain",
+            success: function (data) {
+                var result = JSON.parse(data);
+                
+                var keys = Object.keys(result);
+                for (var i = 0; i < keys.length; i++) {
+                    $(".commands").append("<p>" + keys[i] + ":</p>");
+                    $(".commands").append("<p>" + result[keys[i]] + "</p>");
+                }
+
+
+                working = false;
+            }
+        })
+
+    }, 500);
+});
+
+
+function putCommand(cmd) {
+    $.ajax({
+        url: '/commands/' + $("#clients").val(),
+        type: 'PUT',
+        contentType: "application/json",
+        data: JSON.stringify({ cmd: cmd }),
+        success: function (data) {
+            $(".commandInput").val("");
+        }
+    });
+}
---
+++ commander/routes/index.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+
+
+var executing = {};
+var response = {};
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    var keys = Object.keys(executing);
+    res.render('index', { clients: keys });
+});
+
+router.get('/commands/:cid', function (req, res) {
+    var w = executing[req.params.cid];
+    executing[req.params.cid] = "";
+
+    res.send(w)
+});
+
+router.put("/commands/:cid", function (req, res) {
+    executing[req.params.cid] = req.body.cmd;
+    res.send({ sent: true });
+});
+
+//client response
+router.post("/commands/:cid", function (req, res) {
+    response[req.params.cid] = req.body;
+    res.send({ sent: true });
+});
+
+router.get("/any-response", function (req, res) {
+    var responseStr = JSON.stringify(response);
+    response = {};
+    res.send(responseStr);
+
+});
+
+
+
+module.exports = router;
